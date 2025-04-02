540348d6eb019c70775edf552b6cf021ffea146a
Riv2oo4 <riv22500@uvg.edu.gt>
2025-04-02 03:01:47 -0600

    Clase contacto

--- chat_cliente_gtk.cpp
+++ chat_cliente_gtk.cpp
@@ -14,20 +14,22 @@
 #include <functional>
 #include <algorithm>
 
-
+// Namespace aliases for cleaner code
 namespace net = boost::asio;
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 using tcp = net::ip::tcp;
 
-// Protocolo tipo de mensajes
+// Protocol message types
 enum ProtocolMessageType : uint8_t {
+    // Client to server messages
     MSG_CLIENT_REQUEST_USERS = 1,
     MSG_CLIENT_GET_USER_INFO = 2,
     MSG_CLIENT_UPDATE_STATUS = 3,
     MSG_CLIENT_SEND_MESSAGE = 4,
     MSG_CLIENT_REQUEST_HISTORY = 5,
 
+    // Server to client messages
     MSG_SERVER_ERROR = 50,
     MSG_SERVER_USER_LIST = 51,
     MSG_SERVER_USER_INFO = 52,
@@ -37,6 +39,7 @@ enum ProtocolMessageType : uint8_t {
     MSG_SERVER_CHAT_HISTORY = 56
 };
 
+// Error codes from server
 enum ServerErrorCode : uint8_t {
     ERR_USER_NOT_FOUND = 1,
     ERR_INVALID_STATUS = 2,
@@ -44,6 +47,7 @@ enum ServerErrorCode : uint8_t {
     ERR_RECIPIENT_OFFLINE = 4
 };
 
+// User status
 enum class UserStatus : uint8_t {
     OFFLINE = 0,
     ONLINE = 1,
@@ -51,4 +55,30 @@ enum class UserStatus : uint8_t {
     AWAY = 3
 };
 
-
+class Contact {
+    private:
+        std::string username;
+        UserStatus status;
+    
+    public:
+        Contact() : username(""), status(UserStatus::OFFLINE) {}
+        Contact(std::string username, UserStatus status) 
+            : username(std::move(username)), status(status) {}
+        
+        const std::string& getName() const { return username; }
+        UserStatus getStatus() const { return status; }
+        
+        void setName(const std::string& name) { username = name; }
+        void setStatus(UserStatus newStatus) { status = newStatus; }
+        
+        wxString getFormattedName() const {
+            std::string statusIndicator;
+            switch (status) {
+                case UserStatus::ONLINE: statusIndicator = "[+] "; break;
+                case UserStatus::BUSY: statusIndicator = "[!] "; break;
+                case UserStatus::AWAY: statusIndicator = "[~] "; break;
+                case UserStatus::OFFLINE: statusIndicator = "[-] "; break;
+            }
+            return wxString(statusIndicator + username);
+        }
+    };
